d00322b25adae6cb78e37925e7aa869da8efd09f
Anteneh Kassaw <88293613+Anteneh2121@users.noreply.github.com>
2022-04-26 13:00:50 -0700

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,68 +1,48 @@
 #!/usr/bin/python3
-
+"""queries the Reddit API, parses the title of all hot articles, and prints a
+sorted count of given keywords(case-insensitive, delimited by spaces.
+Javascript should count as javascript, but java should not).
 """
-importing requests module
-"""
-
-from requests import get
+import requests
+import operator
 
 
-def count_words(subreddit, word_list=[], after=None, cleaned_dict=None):
-    """
-    function that queries the Reddit API, parses the title of all hot articles,
-    and prints a sorted count of given keywords (case-insensitive, delimited by
-    spaces. Javascript should count as javascript, but java should not).
-    """
-
-    temp = []
-
-    for i in word_list:
-        temp.append(i.casefold())
-
-    cleaned_word_list = list(dict.fromkeys(temp))
-
-    if cleaned_dict is None:
-        cleaned_dict = dict.fromkeys(cleaned_word_list)
-
-    params = {'show': 'all'}
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return None
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-
-    url = 'https://www.reddit.com/r/{}/hot/.json?after={}'.format(subreddit,
-                                                                  after)
-
-    response = get(url, headers=user_agent, params=params)
-
-    if (response.status_code != 200):
-        return None
-
-    all_data = response.json()
-    raw1 = all_data.get('data').get('children')
-    after = all_data.get('data').get('after')
-
-    if after is None:
-        new = {k: v for k, v in cleaned_dict.items() if v is not None}
-
-        for k in sorted(new.items(), key=lambda x: (-x[1], x[0])):
-            print("{}: {}".format(k[0], k[1]))
-
-        return None
-
-    for i in raw1:
-        title = i.get('data').get('title')
-
-        split_title = title.split()
-
-        split_title2 = [i.casefold() for i in split_title]
-
-        for j in split_title2:
-            if j in cleaned_dict and cleaned_dict[j] is None:
-                cleaned_dict[j] = 1
-
-            elif j in cleaned_dict and cleaned_dict[j] is not None:
-                cleaned_dict[j] += 1
-
-    count_words(subreddit, word_list, after, cleaned_dict)
+def count_words(subreddit, word_list=[], word_dict={}, after=None):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
+        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    }
+    r = requests.get('https://www.reddit.com/r/{:}/hot.json?after={:}'.format(
+        subreddit, after), headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        json = r.json()
+        data_dict = json.get('data')
+        post_list = data_dict.get('children')
+        for post in post_list:
+            post_data_dict = post.get('data')
+            for word in word_list:
+                title = post_data_dict.get('title').split()
+                title_copy = []
+                for j in title:
+                    title_copy.append(j.upper())
+                count = title_copy.count(word.upper())
+                if word_dict.get(word):
+                    word_dict[word] += count
+                else:
+                    word_dict[word] = count
+        after = data_dict.get('after')
+        if data_dict.get('after') is None:
+            sorted_list = sorted(word_dict.items(), key=operator.itemgetter(1),
+                                 reverse=True)
+            for i in range(len(sorted_list) - 1):
+                if sorted_list[i][1] == sorted_list[i+1][1]:
+                    if sorted_list[i][0] > sorted_list[i+1][0]:
+                        sorted_list[i], sorted_list[i+1] = sorted_list[
+                            i+1], sorted_list[i]
+            for item in sorted_list:
+                if item[1] > 0:
+                    print("{:}: {:}".format(item[0], item[1]))
+            return
+        return count_words(subreddit, word_list, word_dict, after)
+    else:
+        return
