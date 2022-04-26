f3ebdc3c5a4ced50998c43139a0d9e2f3640892d
Anteneh Kassaw <88293613+Anteneh2121@users.noreply.github.com>
2022-04-26 13:12:23 -0700

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,47 +1,50 @@
 #!/usr/bin/python3
-"""Module for task 3"""
+"""
+Contains the recurse method
+"""
+from collections import defaultdict
+import random
+import requests
+user_agent = "User Agent{:d}".format(random.randrange(1000, 9999))
+header = {'User-Agent': user_agent}
 
 
-def count_words(subreddit, word_list, word_count={}, after=None):
-    """Queries the Reddit API and returns the count of words in
-    word_list in the titles of all the hot posts
-    of the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
-                            .format(subreddit),
-                            params={"after": after},
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code != 200:
-        return None
-
-    info = sub_info.json()
-
-    hot_l = [child.get("data").get("title")
-             for child in info
-             .get("data")
-             .get("children")]
-    if not hot_l:
-        return None
-
-    word_list = list(dict.fromkeys(word_list))
-
-    if word_count == {}:
-        word_count = {word: 0 for word in word_list}
-
-    for title in hot_l:
-        split_words = title.split(' ')
-        for word in word_list:
-            for s_word in split_words:
-                if s_word.lower() == word.lower():
-                    word_count[word] += 1
-
-    if not info.get("data").get("after"):
-        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
-        sorted_counts = sorted(word_count.items(),
-                               key=lambda kv: kv[1], reverse=True)
-        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
+def count_words(subreddit, word_list, base=0, keywords={}, after=""):
+    """
+    Creates a list of all the titles of the hot list
+    input:
+        subreddit: the name of a reddit subreddit
+        hot_list: list to be filled with the sub reddit name
+    return: host_list or None otherwise
+    """
+    if keywords == {}:
+        word_list = [word.lower() for word in word_list]
+        word_list = list(set(word_list))
+        keywords = defaultdict(int)
+    url = "https://www.reddit.com/r/{}/.json".format(subreddit)
+    r = requests.get(url, headers=header, params={"after": after},
+                     allow_redirects=False)
+    if r.status_code == 200:
+        r_dict = r.json()
+        for reddit_post in r_dict["data"]["children"]:
+            title_words = reddit_post['data']['title'].lower().split()
+            for title_word in title_words:
+                for word in word_list:
+                    if word == title_word:
+                        keywords[word] += 1
+        after = r_dict['data']['after']
+        if after is None:
+            return keywords
+        else:
+            count_words(subreddit, word_list,
+                                   base + 1, keywords, after)
     else:
-        return count_words(subreddit, word_list, word_count,
-                           info.get("data").get("after"))
+        return None
+    # only to be executed for before the recursive
+    if base is 0:
+        keywords_list = list(keywords.items())
+        keywords_list = sorted(keywords_list, key=lambda x: x[1], reverse=True)
+        for pair in keywords_list:
+            if pair[1] is not 0:
+                print("{}: {}".format(pair[0], pair[1]))
+    return keywords
